cf63d999fef21ed8d1c933e50d1788b6375bbec8
Pattuv <161959486+Pattuv@users.noreply.github.com>
2025-12-21 21:35:34 -0500

    initialization of external dependencies

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,5 +1,12 @@
 function Test() {
-  return <h1 style={{ color: "red" }}>This is a test component. It is Red.</h1>;
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <button className="btn btn-ghost text-xl bric">Theo.ai</button>
+      <button className="inter text-xs text-neutral-content">
+        Partial Blindess Accessibility Agent
+      </button>
+    </div>
+  );
 }
 
 export default Test;
--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import Test from "./components/test";
 
 const App = () => {
