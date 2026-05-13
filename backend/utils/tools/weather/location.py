237b0525022e09341723553252c7e9f916240992
Pattuv <161959486+Pattuv@users.noreply.github.com>
2026-05-13 14:27:49 -0400

    done with weather tool. Time to integrate it into Theo

--- backend/utils/tools/weather/location.py
+++ backend/utils/tools/weather/location.py
@@ -5,7 +5,6 @@ def getLocation():
     r = requests.get("https://ipinfo.io/json", timeout=5)
     r.raise_for_status()
     data = r.json()
-    return (f"{data.get("city"), data.get("country")}")  
-
-#development scaffolding
-print(getLocation())
+    city = data.get("city")
+    country = data.get("country")
+    return f"{city}, {country}"
